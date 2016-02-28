3fc6aa5e4e456a35bbfb7e1eaeea8b8dbbc472b2
andresmrllo <andresmrllo@gmail.com>
2016-02-28 11:49:11 -0600

    Modifique el orden de la matriz!! Solo se recibe un numero!!

--- Descomposicion de matrices/src/descomposicion/de/matrices/Matriz.java
+++ Descomposicion de matrices/src/descomposicion/de/matrices/Matriz.java
@@ -17,11 +17,9 @@ public class Matriz {
     
     public void cargar() {
         teclado=new Scanner(System.in);
-        System.out.print("Cuantas filas tiene la matriz:");
-        int filas=teclado.nextInt();
-        System.out.print("Cuantas columnas tiene la matriz:");
-        int columnas=teclado.nextInt();
-        matriz=new int[filas][columnas];
+        System.out.print("Cuantas filas y columnas tiene la matriz:");
+        int orden =teclado.nextInt();
+        matriz=new int[orden][orden];
         for(int f=0;f<matriz.length;f++) {
             for(int c=0;c<matriz[f].length;c++) {
                 System.out.print("Ingrese componente:");
