731b87452cb64ea9d4caac291c5b6929364cfcd8
andresmrllo <andresmrllo@gmail.com>
2016-03-02 17:11:06 -0600

    Menu principal y funcional terminado!

--- Descomposicion de matrices/src/descomposicion/de/matrices/Principal.java
+++ Descomposicion de matrices/src/descomposicion/de/matrices/Principal.java
@@ -5,6 +5,9 @@
  */
 package descomposicion.de.matrices;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  *
  * @author Andresmrllo
@@ -14,16 +17,80 @@ public class Principal {
     /**
      * @param args the command line arguments
      */
+    
+        public static void promptEnterKey(){        
+        try {            
+            System.out.println("Presione \"ENTER\" para continuar...");
+            int read = System.in.read(new byte[2]);
+            limpiarConsola();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    private static void limpiarConsola(){
+        for(int clear = 0; clear < 50; clear++)
+        {
+           System.out.println("\b") ;
+        }
+    }
+    
+    public static void menu()
+    {
+        
+        System.out.println("\tDigite la opción que desea realizar");
+        System.out.println("\t\t1)Crear suma de matrices");
+        System.out.println("\t\t2)Salir\n");
+        
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        cualMetodo(num);
+    }
+    
+    public static void cualMetodo(int seleccion)
+    {
+        Matriz ma;
+        int[][] transpuesta ;
+        
+        switch(seleccion) 
+        {
+            case 1:
+                ma = new Matriz();
+                ma.cargar();
+                transpuesta = ma.crearTranspuesta();
+                System.out.println("Matriz creada");
+                ma.imprimir(ma.matriz);
+                System.out.println("Su matriz es igual a la suma de:\n");
+                ma.crearMatrizSimetrica(transpuesta);
+                System.out.println("\n+\n");
+                ma.crearMatrizAntiSimetrica(transpuesta);
+                System.out.println("\n----------------------------\n");
+                menu();
+                break;
+            case 2:
+                promptEnterKey();
+                System.out.println("--> CERRANDO SISTEMA \n\n\t¡Hasta pronto!\n");
+                System.exit(0);
+                break;
+        default: 
+            System.out.println("Error: Ha digitado una opción no válida.");
+            promptEnterKey();
+            menu();
+            break;
+        }
+    }
+    
     public static void main(String[] args) 
     {
+        System.out.println("Bienvenido");
+        menu();
+        /*
         // TODO code application logic here
         Matriz ma=new Matriz();
         ma.cargar();
         ma.imprimir(ma.matriz);
        // ma.imprimirUltimaFila();
-        int[][] transpuesta = ma.crearTranspuesta();
-        ma.crearMatrizSimetrica(transpuesta);
-        ma.crearMatrizAntiSimetrica(transpuesta);
+        int[][] transpuesta = ma.crearTranspuesta();*/
     } 
  }
     
